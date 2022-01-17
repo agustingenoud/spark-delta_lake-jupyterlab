1755b041d2be638b85655a27fa05c1443d45b630
agustingenoud <agustingenoud@gmail.com>
2022-01-17 17:03:09 -0300

    updated to openjdk 11, spark 3.2.0 & hadoop 3.2

--- 00-build-images/build-images.sh
+++ 00-build-images/build-images.sh
@@ -2,32 +2,32 @@
 
 # -- Software Stack Version
 
-SPARK_VERSION="3.0.0"
-HADOOP_VERSION="2.7"
+SPARK_VERSION="3.2.0"
+HADOOP_VERSION="3.2"
 JUPYTERLAB_VERSION="2.1.5"
 
 # -- Building the Images
 
 docker build \
   -f ./00-cluster-base/Dockerfile \
-  -t cluster-base .
+  -t cluster-base-t .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
   -f ./01-spark-base/Dockerfile \
-  -t spark-base .
+  -t spark-base-t .
 
 docker build \
   -f ./02-spark-master/Dockerfile \
-  -t spark-master .
+  -t spark-master-t .
 
 docker build \
   -f ./03-spark-worker/Dockerfile \
-  -t spark-worker .
+  -t spark-worker-t .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
   -f ./04-jupyterlab/Dockerfile \
-  -t jupyterlab .
+  -t jupyterlab-t .
