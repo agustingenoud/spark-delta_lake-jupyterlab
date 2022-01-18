aa3bfe93d2fc1e01be3ca1c3444616d6b0e80c40
agustingenoud <agustingenoud@gmail.com>
2022-01-18 10:39:23 -0300

    remove t > test images

--- 00-build-images/build-images.sh
+++ 00-build-images/build-images.sh
@@ -10,24 +10,24 @@ JUPYTERLAB_VERSION="2.1.5"
 
 docker build \
   -f ./00-cluster-base/Dockerfile \
-  -t cluster-base-t .
+  -t cluster-base .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
   -f ./01-spark-base/Dockerfile \
-  -t spark-base-t .
+  -t spark-base .
 
 docker build \
   -f ./02-spark-master/Dockerfile \
-  -t spark-master-t .
+  -t spark-master .
 
 docker build \
   -f ./03-spark-worker/Dockerfile \
-  -t spark-worker-t .
+  -t spark-worker .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
   -f ./04-jupyterlab/Dockerfile \
-  -t jupyterlab-t .
+  -t jupyterlab .
